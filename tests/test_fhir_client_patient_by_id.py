f92d20bd79334bf9392373eba0a13d882da32a88
imranq2 <imranq2@hotmail.com>
2021-03-30 11:32:40 -0700

    add simple test

--- tests/test_fhir_client_patient_by_id.py
+++ tests/test_fhir_client_patient_by_id.py
@@ -6,14 +6,14 @@ from helix_fhir_client_sdk.fhir_client import FhirClient
 from helix_fhir_client_sdk.fhir_request_response import FhirRequestResponse
 
 
-def test_fhir_client_simple() -> None:
+def test_fhir_client_patient_by_id() -> None:
     adapter = requests_mock.Adapter()
     url = "http://foo"
     response_text = {"resourceType": "Patient", "id": "12355"}
-    adapter.register_uri("GET", f"{url}/Patient", text=json.dumps(response_text))
+    adapter.register_uri("GET", f"{url}/Patient/12355", text=json.dumps(response_text))
 
     fhir_client = FhirClient()
-    fhir_client = fhir_client.url(url).resource("Patient")
+    fhir_client = fhir_client.url(url).resource("Patient").id_("12355")
     fhir_client = fhir_client.adapter(adapter)
     response: FhirRequestResponse = fhir_client.send_request()
 
---
+++ tests/test_fhir_client_patient_list.py
@@ -0,0 +1,21 @@
+import json
+
+import requests_mock
+
+from helix_fhir_client_sdk.fhir_client import FhirClient
+from helix_fhir_client_sdk.fhir_request_response import FhirRequestResponse
+
+
+def test_fhir_client_patient_list() -> None:
+    adapter = requests_mock.Adapter()
+    url = "http://foo"
+    response_text = {"resourceType": "Patient", "id": "12355"}
+    adapter.register_uri("GET", f"{url}/Patient", text=json.dumps(response_text))
+
+    fhir_client = FhirClient()
+    fhir_client = fhir_client.url(url).resource("Patient")
+    fhir_client = fhir_client.adapter(adapter)
+    response: FhirRequestResponse = fhir_client.send_request()
+
+    print(response.responses)
+    assert response.responses == [json.dumps(response_text)]
