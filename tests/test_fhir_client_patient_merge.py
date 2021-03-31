eb359b1e8a6777c31592759438a954e11af741c6
imranq2 <imranq2@hotmail.com>
2021-03-31 14:47:40 -0700

    add test for merge

--- tests/test_fhir_client_patient_merge.py
+++ tests/test_fhir_client_patient_merge.py
@@ -1,6 +1,9 @@
 import json
+from typing import Any, Optional
 
+import requests
 import requests_mock
+from requests import Response
 
 from helix_fhir_client_sdk.fhir_client import FhirClient
 from helix_fhir_client_sdk.responses.fhir_merge_response import FhirMergeResponse
@@ -11,7 +14,21 @@ def test_fhir_client_patient_merge() -> None:
         url = "http://foo"
         request_data = {"resourceType": "Patient", "id": "12355"}
         response_json = [{"created": 1, "updated": 0}]
-        mock.post(f"{url}/Patient/1/$merge", json=response_json)
+
+        def custom_matcher(request: Any) -> Optional[Response]:
+            if request.path == "/patient/1/$merge" and request.text == json.dumps(
+                [request_data]
+            ):
+                resp: Response = requests.Response()
+                resp.status_code = 200
+                return resp
+            return None
+
+        mock.post(
+            f"{url}/Patient/1/$merge",
+            additional_matcher=custom_matcher,
+            text=f"{json.dumps(response_json)}",
+        )
 
         fhir_client = FhirClient()
         fhir_client = fhir_client.url(url).resource("Patient")
