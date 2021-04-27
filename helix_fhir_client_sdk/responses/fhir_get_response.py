f4d578dbdc8bb85c39c1757ca99a86fca0d65b2c
Gagan Chawla <gagan.chawla@icanbwell.com>
2021-04-27 20:04:54 +0530

    Updated FhirGetResponse.responses format to be consistent with requests' response

--- helix_fhir_client_sdk/responses/fhir_get_response.py
+++ helix_fhir_client_sdk/responses/fhir_get_response.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 
 class FhirGetResponse:
-    def __init__(self, url: str, responses: List[str], error: Optional[str]) -> None:
+    def __init__(self, url: str, responses: str, error: Optional[str]) -> None:
         self.url: str = url
-        self.responses: List[str] = responses
+        self.responses: str = responses
         self.error: Optional[str] = error
