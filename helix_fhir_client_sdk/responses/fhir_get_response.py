db08571b6bbb6b4128480f963e99e3c4cb27d734
imranq2 <imranq2@hotmail.com>
2021-05-10 18:08:32 -0700

    return access token and cache auth url

--- helix_fhir_client_sdk/responses/fhir_get_response.py
+++ helix_fhir_client_sdk/responses/fhir_get_response.py
@@ -2,7 +2,14 @@ from typing import Optional
 
 
 class FhirGetResponse:
-    def __init__(self, url: str, responses: str, error: Optional[str]) -> None:
+    def __init__(
+        self,
+        url: str,
+        responses: str,
+        error: Optional[str],
+        access_token: Optional[str],
+    ) -> None:
         self.url: str = url
         self.responses: str = responses
         self.error: Optional[str] = error
+        self.access_token: Optional[str] = access_token
