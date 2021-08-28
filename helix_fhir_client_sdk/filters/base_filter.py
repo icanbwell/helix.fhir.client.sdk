03d5c1a4ff2452b70301ce7a70e4f9b64bdfdc3b
imranq2 <imranq2@hotmail.com>
2021-08-28 16:53:12 -0700

    update documentation

--- helix_fhir_client_sdk/filters/base_filter.py
+++ helix_fhir_client_sdk/filters/base_filter.py
@@ -2,4 +2,7 @@ class BaseFilter:
     pass
 
     def __str__(self) -> str:
+        """
+        Returns the query parameter representation to send to the FHIR server
+        """
         raise NotImplementedError
--- helix_fhir_client_sdk/filters/identifier_filter.py
+++ helix_fhir_client_sdk/filters/identifier_filter.py
@@ -4,7 +4,8 @@ from helix_fhir_client_sdk.filters.base_filter import BaseFilter
 class IdentifierFilter(BaseFilter):
     def __init__(self, system: str, value: str) -> None:
         """
-        Filter for finding records where there is an identifier with sytem and value
+        Restrict results to only records that have an identifier with this system and value
+        Example: system= http://hl7.org/fhir/sid/us-npi, value= 1487831681
 
 
         :param system: system of identifier.  Note that this is the assigning system NOT the coding system
