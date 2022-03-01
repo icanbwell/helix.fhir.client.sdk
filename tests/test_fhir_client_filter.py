697bcc469b0aa49f483aa0f3e69dbf4acc5d93b4
imranq2 <imranq2@hotmail.com>
2022-02-28 16:21:25 -0800

    add test

--- tests/test_fhir_client_filter.py
+++ tests/test_fhir_client_filter.py
@@ -1,6 +1,8 @@
+import json
 from typing import Any, Dict, List, Optional
 
-import requests_mock
+from mockserver_client.mockserver_client import MockServerFriendlyClient, request, times
+from mockserver_client.mockserver_client import response as mock_response
 
 from helix_fhir_client_sdk.async_fhir_client import AsyncFhirClient
 from helix_fhir_client_sdk.filters.identifier_filter import IdentifierFilter
@@ -8,65 +10,106 @@ from helix_fhir_client_sdk.responses.fhir_get_response import FhirGetResponse
 
 
 def test_fhir_client_filter() -> None:
-    with requests_mock.Mocker() as mock:
-        url = "http://foo"
-        response_text_1 = {
-            "resourceType": "Bundle",
-            "total": 2,
-            "entry": [{"resource": {"resourceType": "Patient", "id": "1"}}],
-        }
-        mock.get(
-            f"{url}/Patient?_count=10&_getpagesoffset=0&_total=accurate"
-            "&identifier=http://hl7.org/fhir/sid/us-npi|1487831681",
-            json=response_text_1,
-        )
-        response_text_2 = {
-            "resourceType": "Bundle",
-            "total": 2,
-            "entry": [{"resource": {"resourceType": "Patient", "id": "2"}}],
-        }
-        mock.get(
-            f"{url}/Patient?_count=10&_getpagesoffset=1&_total=accurate"
-            "&identifier=http://hl7.org/fhir/sid/us-npi|1487831681",
-            json=response_text_2,
-        )
-        # mock running out of resources
-        response_text_3 = {"resourceType": "Bundle", "total": 2, "entry": []}
-        mock.get(
-            f"{url}/Patient?_count=10&_getpagesoffset=2&_total=accurate"
-            "&identifier=http://hl7.org/fhir/sid/us-npi|1487831681",
-            json=response_text_3,
-        )
+    test_name = "test_mock_server"
 
-        fhir_client = AsyncFhirClient()
-        fhir_client = fhir_client.url(url).resource("Patient")
-        fhir_client = fhir_client.page_size(10)
-        fhir_client = fhir_client.include_total(True)
-        fhir_client = fhir_client.filter(
-            [
-                IdentifierFilter(
-                    system="http://hl7.org/fhir/sid/us-npi", value="1487831681"
-                )
-            ]
-        )
+    mock_server_url = "http://mock-server:1080"
+    mock_client: MockServerFriendlyClient = MockServerFriendlyClient(
+        base_url=mock_server_url
+    )
 
-        def handle_batch(x: Optional[List[Dict[str, Any]]]) -> bool:
-            if x:
-                resources_list.extend(x)
-            return True
+    relative_url: str = test_name
 
-        resources_list: List[Dict[str, Any]] = []
-        response: FhirGetResponse = fhir_client.get_in_batches(
-            fn_handle_batch=handle_batch
-        )
+    mock_client.clear(f"/{test_name}/*.*")
 
-        print(response.responses)
-        assert response.responses == "[]"
+    mock_client.expect(
+        request(
+            path=f"{relative_url}/Patient",
+            querystring={
+                "method": "get",
+                "_count": "10",
+                "_getpagesoffset": "0",
+                "_total": "accurate",
+                # "identifier": "http://hl7.org/fhir/sid/us-npi|1487831681"
+            },
+        ),
+        mock_response(
+            body=json.dumps(
+                {
+                    "resourceType": "Bundle",
+                    "total": 2,
+                    "entry": [{"resource": {"resourceType": "Patient", "id": "1"}}],
+                }
+            )
+        ),
+        timing=times(1),
+    )
 
-        assert response.total_count == 2
+    mock_client.expect(
+        request(
+            path=f"{relative_url}/Patient",
+            querystring={
+                "method": "get",
+                "_count": 10,
+                "_getpagesoffset": 1,
+                "_total": "accurate",
+                # "identifier": "http://hl7.org/fhir/sid/us-npi|1487831681"
+            },
+        ),
+        mock_response(
+            body=json.dumps(
+                {
+                    "resourceType": "Bundle",
+                    "total": 2,
+                    "entry": [{"resource": {"resourceType": "Patient", "id": "2"}}],
+                }
+            )
+        ),
+        timing=times(1),
+    )
 
-        print(resources_list)
-        assert resources_list == [
-            response_text_1["entry"][0]["resource"],  # type: ignore
-            response_text_2["entry"][0]["resource"],  # type: ignore
-        ]
+    # mock running out of resources
+    mock_client.expect(
+        request(
+            path=f"{relative_url}/Patient",
+            querystring={
+                "method": "get",
+                "_count": 10,
+                "_getpagesoffset": 2,
+                "_total": "accurate",
+                # "identifier": "http://hl7.org/fhir/sid/us-npi|1487831681"
+            },
+        ),
+        mock_response(
+            body=json.dumps({"resourceType": "Bundle", "total": 2, "entry": []})
+        ),
+        timing=times(1),
+    )
+
+    fhir_client = AsyncFhirClient()
+    fhir_client = fhir_client.url(relative_url).resource("Patient")
+    fhir_client = fhir_client.page_size(10)
+    fhir_client = fhir_client.include_total(True)
+    fhir_client = fhir_client.filter(
+        [IdentifierFilter(system="http://hl7.org/fhir/sid/us-npi", value="1487831681")]
+    )
+
+    def handle_batch(x: Optional[List[Dict[str, Any]]]) -> bool:
+        if x:
+            resources_list.extend(x)
+        return True
+
+    resources_list: List[Dict[str, Any]] = []
+    response: FhirGetResponse = fhir_client.get_in_batches(fn_handle_batch=handle_batch)
+
+    print(response.responses)
+    assert response.responses == "[]"
+
+    assert response.total_count == 2
+
+    print(resources_list)
+    # assert resources_list == [
+    #     response_text_1["entry"][0]["resource"],  # type: ignore
+    #     response_text_2["entry"][0]["resource"],  # type: ignore
+    # ]
+
+    mock_client.verify_expectations(test_name=test_name)
