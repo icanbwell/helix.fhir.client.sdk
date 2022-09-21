730f0273449c9f2fad22d9ea0e6e1d175e43db4b
imranq2 <imranq2@hotmail.com>
2022-09-20 17:09:55 -0700

    log parameters

--- helix_fhir_client_sdk/exceptions/fhir_validation_exception.py
+++ helix_fhir_client_sdk/exceptions/fhir_validation_exception.py
@@ -24,5 +24,5 @@ class FhirValidationException(Exception):
         self.url: str = url
         self.data: str = json_data
         super().__init__(
-            f"FHIR send validation failed: {url} {response_status_code}: {json_data}.  {message} {response_text}"
+            f"FHIR send  {request_id} validation failed: {url} {response_status_code}: {json_data}.  {message} {response_text}"
         )
