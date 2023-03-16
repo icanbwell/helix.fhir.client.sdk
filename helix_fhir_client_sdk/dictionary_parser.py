c9433f39622ee4ceedd928e941ab1dd1d1ffd934
imranq2 <imranq2@hotmail.com>
2023-03-15 21:23:01 -0700

    add check that reference exists

--- helix_fhir_client_sdk/dictionary_parser.py
+++ helix_fhir_client_sdk/dictionary_parser.py
@@ -22,28 +22,35 @@ class DictionaryParser:
     @staticmethod
     def get_nested_property(
         parent: Dict[str, Any], path: str
-    ) -> Union[List[Dict[str, Any]], Dict[str, Any], str]:
+    ) -> Union[List[Dict[str, Any]], Dict[str, Any], str, None]:
+        if parent is None:
+            return None
         parts: List[str] = path.split(".")
         result: Union[List[Dict[str, Any]], Dict[str, Any]] = parent
         for part in parts:
+            if result is None:
+                return None
             if part.endswith("[x]"):  # list
                 clean_part = part[:-3]
                 if isinstance(result, list):
                     new_result: List[Dict[str, Any]] = []
                     for result_entry in result:
-                        new_result.append(result_entry.get(clean_part))  # type: ignore
+                        value = result_entry.get(clean_part)
+                        if value is not None:
+                            new_result.append(value)
                     result = new_result
                 else:
-                    result = result.get(clean_part)  # type: ignore
+                    result = result.get(clean_part, None)
             else:
                 if isinstance(result, list):
                     result = DictionaryParser.flatten(
                         [
                             DictionaryParser.get_nested_property(parent=r, path=part)
                             for r in result
+                            if r is not None
                         ]
                     )
                 else:
-                    result = result.get(part)  # type: ignore
+                    result = result.get(part, None)
 
         return result
