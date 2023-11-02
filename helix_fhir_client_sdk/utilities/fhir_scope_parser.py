7ce57f39a9b3e6649608a50d2be501ed1facee61
Imran Qureshi <imranq2@hotmail.com>
2023-11-02 14:31:47 -0700

    skip calls for resources if the scope does not include the resource type

--- helix_fhir_client_sdk/utilities/fhir_scope_parser.py
+++ helix_fhir_client_sdk/utilities/fhir_scope_parser.py
@@ -6,6 +6,12 @@ from helix_fhir_client_sdk.utilities.fhir_scope_parser_result import (
 
 
 class FhirScopeParser:
+    def __init__(self, scopes: Optional[str]) -> None:
+        self.scopes = scopes
+        self.parsed_scopes: Optional[List[FhirScopeParserResult]] = (
+            self.parse_scopes(scopes) if scopes else None
+        )
+
     @staticmethod
     def parse_scopes(scopes: Optional[str]) -> List[FhirScopeParserResult]:
         if not scopes:
@@ -44,3 +50,37 @@ class FhirScopeParser:
                 )
 
         return parsed_scopes
+
+    def scope_allows(self, resource_type: str, interaction: str = "read") -> bool:
+        """
+        Returns whether the scope allows the given resource type and interaction.
+
+
+        :param resource_type: The resource type to check.
+        :param interaction: The interaction to check.
+        :return: True if the scope allows the given resource type and interaction, False otherwise.
+        """
+        if not self.parsed_scopes:
+            return True
+        assert resource_type
+        assert interaction
+
+        # note that resource_type is actually called Operation in SMART on FHIR scopes:
+        # A SMART on FHIR scope string is constructed as resourceType.operation.permission.
+        # For example, the scope string patient/*.read requests read-only access to all patient-related resources.
+        scope: FhirScopeParserResult
+        for scope in self.parsed_scopes:
+            if (
+                scope.operation
+                and scope.interaction
+                and (
+                    scope.operation == "*"
+                    or scope.operation.lower() == resource_type.lower()
+                )
+                and (
+                    scope.interaction == "*"
+                    or scope.interaction.lower() == interaction.lower()
+                )
+            ):
+                return True
+        return False
