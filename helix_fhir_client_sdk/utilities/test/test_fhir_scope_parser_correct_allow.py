b7b9c42bc3b4e56d10c3454319409077193e60b9
Drew Simmons <drew.simmons@icanbwell.com>
2024-03-26 14:40:12 -0400

    add test for removing invalid launch/patient scope

--- helix_fhir_client_sdk/utilities/test/test_fhir_scope_parser_correct_allow.py
+++ helix_fhir_client_sdk/utilities/test/test_fhir_scope_parser_correct_allow.py
@@ -1,5 +1,9 @@
 from helix_fhir_client_sdk.utilities.fhir_scope_parser import FhirScopeParser
 
+from helix_fhir_client_sdk.utilities.fhir_scope_parser_result import (
+    FhirScopeParserResult,
+)
+
 
 def test_fhir_scope_parser_correct_allow() -> None:
     scope_parser: FhirScopeParser = FhirScopeParser(
@@ -11,20 +15,32 @@ patient/AllergyIntolerance.read patient/Binary.read patient/CarePlan.read patien
     )
 
     assert (
-        scope_parser.scope_allows(resource_type="Patient", interaction="read") is True
+            scope_parser.scope_allows(resource_type="Patient", interaction="read") is True
     )
 
     assert (
-        scope_parser.scope_allows(resource_type="Condition", interaction="read") is True
+            scope_parser.scope_allows(resource_type="Condition", interaction="read") is True
     )
 
     assert (
-        scope_parser.scope_allows(
-            resource_type="MedicationDispense", interaction="read"
-        )
-        is False
+            scope_parser.scope_allows(
+                resource_type="MedicationDispense", interaction="read"
+            )
+            is False
     )
 
     assert (
-        scope_parser.scope_allows(resource_type="Patient", interaction="write") is False
+            scope_parser.scope_allows(resource_type="Patient", interaction="write") is False
     )
+
+
+def test_fhir_scope_parser_remove_invalid_launch_patient() -> None:
+    no_patient_scope_parser: FhirScopeParser = FhirScopeParser(
+        scopes=[
+            """
+launch/patient offline_access openid profile user/AllergyIntolerance.read user/Appointment.read user/CarePlan.read user/Encounter.read user/Immunization.read user/MedicationAdministration.read user/MedicationRequest.read user/Organization.read user/Practitioner.read user/Schedule.read user/Slot.read"""
+        ]
+    )
+
+    assert (FhirScopeParserResult(resource_type='launch', operation=None, interaction='patient',
+                                  scope=None) not in no_patient_scope_parser.parsed_scopes)
