9557fdb9845e2e3c15f9eb8f958f89cb21113e62
Vibhor Agarwal <vibhor.agarwal@icanbwell.com>
2024-07-18 16:55:01 +0530

    DFP-3286 support for sending additional request headers added

--- tests/async/test_async_fhir_client_patient_delete.py
+++ tests/async/test_async_fhir_client_patient_delete.py
@@ -28,10 +28,15 @@ async def test_fhir_client_patient_delete_async() -> None:
         mock_response(code=204),
         timing=times(1),
     )
+    additional_request_headers = {
+        "TestHeaderOne": "abcdtest",
+        "User-Agent": "TestPipelineName",
+    }
 
     # Act
     fhir_client = FhirClient()
     fhir_client = fhir_client.url(absolute_url).resource("Patient").id_("12345")
+    fhir_client = fhir_client.additional_request_headers(additional_request_headers)
     response: FhirDeleteResponse = await fhir_client.delete_async()
 
     # Assert
