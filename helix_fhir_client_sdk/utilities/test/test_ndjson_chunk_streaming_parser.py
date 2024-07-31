1289d19d8f59f8b12f211088e15dccfbd0fea2bb
Imran Qureshi <imranq2@hotmail.com>
2024-07-31 12:47:45 -0700

    add tests

--- helix_fhir_client_sdk/utilities/test/test_ndjson_chunk_streaming_parser.py
+++ helix_fhir_client_sdk/utilities/test/test_ndjson_chunk_streaming_parser.py
@@ -9,12 +9,14 @@ def test_ndjson_chunk_streaming_parser() -> None:
     print("")
     data_chunks = [
         '{"name": "John", "age": 30}\n{"name": "Jane", "a',
-        'ge": 25}\n{"name": "Doe", "age": 40}\n{"name": "Smith", "age": 35}\n',
+        'ge": 25}\n{"name": "Doe", "age": 40}\n{"name":',
+        ' "Smith", "age": 35}\n',
     ]
 
     parser = NdJsonChunkStreamingParser()
 
     all_objects: List[Dict[str, Any]] = []
+    all_objects_by_chunk: List[List[Dict[str, Any]]] = []
 
     chunk_number = 0
     for chunk in data_chunks:
@@ -22,11 +24,13 @@ def test_ndjson_chunk_streaming_parser() -> None:
         complete_json_objects = parser.add_chunk(chunk)
         print(f"{chunk_number}: ", complete_json_objects)
         all_objects.extend(complete_json_objects)
+        all_objects_by_chunk.append(complete_json_objects)
 
     # Finalize to process any remaining data in the buffer
     remaining_objects = parser.finalize()
     print("Remaining JSON objects from buffer:", remaining_objects)
     all_objects.extend(remaining_objects)
+    all_objects_by_chunk.append(remaining_objects)
 
     print("All JSON objects:", all_objects)
     assert len(all_objects) == 4
@@ -34,3 +38,13 @@ def test_ndjson_chunk_streaming_parser() -> None:
     assert all_objects[1] == {"name": "Jane", "age": 25}
     assert all_objects[2] == {"name": "Doe", "age": 40}
     assert all_objects[3] == {"name": "Smith", "age": 35}
+
+    assert len(all_objects_by_chunk) == 4
+    assert len(all_objects_by_chunk[0]) == 1
+    assert all_objects_by_chunk[0][0] == {"name": "John", "age": 30}
+    assert len(all_objects_by_chunk[1]) == 2
+    assert all_objects_by_chunk[1][0] == {"name": "Jane", "age": 25}
+    assert all_objects_by_chunk[1][1] == {"name": "Doe", "age": 40}
+    assert len(all_objects_by_chunk[2]) == 1
+    assert all_objects_by_chunk[2][0] == {"name": "Smith", "age": 35}
+    assert len(all_objects_by_chunk[3]) == 0
