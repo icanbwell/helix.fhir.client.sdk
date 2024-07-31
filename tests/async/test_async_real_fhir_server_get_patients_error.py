9ddb79ad3fff1421c88c760e47bf1b4b5ea7c4ce
Imran Qureshi <imranq2@hotmail.com>
2024-07-31 15:53:22 -0700

    fix test

--- tests/async/test_async_real_fhir_server_get_patients_error.py
+++ tests/async/test_async_real_fhir_server_get_patients_error.py
@@ -1,20 +1,33 @@
 import json
+from os import environ
 
 from helix_fhir_client_sdk.fhir_client import FhirClient
 from helix_fhir_client_sdk.responses.fhir_merge_response import FhirMergeResponse
 from helix_fhir_client_sdk.utilities.fhir_server_helpers import FhirServerHelpers
 
 
-async def test_dev_server_get_patients_error_async() -> None:
+async def test_async_real_fhir_server_get_patients_error() -> None:
     await FhirServerHelpers.clean_fhir_server_async(resource_type="Patient")
 
-    url = "http://fhir:3000/4_0_0"
+    fhir_server_url: str = environ["FHIR_SERVER_URL"]
+    auth_client_id = environ["FHIR_CLIENT_ID"]
+    auth_client_secret = environ["FHIR_CLIENT_SECRET"]
+    auth_well_known_url = environ["AUTH_CONFIGURATION_URI"]
+
     fhir_client = FhirClient()
-    fhir_client = fhir_client.url(url).resource("Patient")
+    fhir_client = fhir_client.client_credentials(
+        client_id=auth_client_id, client_secret=auth_client_secret
+    )
+    fhir_client = fhir_client.auth_wellknown_url(auth_well_known_url)
+    fhir_client = fhir_client.url(fhir_server_url).resource("Patient")
     await fhir_client.id_("12356").delete_async()
 
     fhir_client = FhirClient()
-    fhir_client = fhir_client.url(url).resource("Patient")
+    fhir_client = fhir_client.client_credentials(
+        client_id=auth_client_id, client_secret=auth_client_secret
+    )
+    fhir_client = fhir_client.auth_wellknown_url(auth_well_known_url)
+    fhir_client = fhir_client.url(fhir_server_url).resource("Patient")
     resource = {
         "resourceType": "Patient",
         "id": "12356",
