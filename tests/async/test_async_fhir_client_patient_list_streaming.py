822ff26f94a125253f1321fe8b3b0ad24b18c1bf
Imran Qureshi <imranq2@hotmail.com>
2024-08-02 14:05:09 -0700

    add ndjson content type to response

--- tests/async/test_async_fhir_client_patient_list_streaming.py
+++ tests/async/test_async_fhir_client_patient_list_streaming.py
@@ -49,7 +49,9 @@ async def test_fhir_client_patient_list_async_streaming() -> None:
             method="GET",
             headers={"Accept": "application/fhir+ndjson"},
         ),
-        response=mock_response(body=response_text),
+        response=mock_response(
+            body=response_text, headers={"Content-Type": "application/fhir+ndjson"}
+        ),
         timing=times(2),
     )
 
