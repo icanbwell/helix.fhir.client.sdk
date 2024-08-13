b048ba42556827f5dbf017491abf3737f4eda4d0
Imran Qureshi <imranq2@hotmail.com>
2024-08-12 20:31:01 -0700

    add run_in_new_thread_and_wait

--- helix_fhir_client_sdk/utilities/async_runner.py
+++ helix_fhir_client_sdk/utilities/async_runner.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 import time
 from typing import Coroutine, Any, TypeVar, Optional
 
@@ -44,13 +45,45 @@ class AsyncRunner:
             # print(f"Ran loop")
         except RuntimeError as e:
             if "This event loop is already running" in str(e):
-                raise RuntimeError(
-                    f"While calling {fn.__name__} there is already an event loop running."
-                    "\nThis usually happens because you are calling this function"
-                    " from an asynchronous context so you can't just wrap it in AsyncRunner.run()."
-                    f"\nEither use `await {fn.__name__}` or"
-                    " use nest_asyncio (https://github.com/erdewit/nest_asyncio)."
-                )
+                try:
+                    return AsyncRunner.run_in_new_thread_and_wait(coro=fn)
+                except RuntimeError as e2:
+                    raise RuntimeError(
+                        f"While calling {fn.__name__} there is already an event loop running."
+                        "\nThis usually happens because you are calling this function"
+                        " from an asynchronous context so you can't just wrap it in AsyncRunner.run()."
+                        f"\nEither use `await {fn.__name__}` or"
+                        " use nest_asyncio (https://github.com/erdewit/nest_asyncio)."
+                        f"\nException: {e}"
+                    )
             else:
                 raise e
         return result
+
+    @staticmethod
+    def run_in_new_thread_and_wait(coro: Coroutine[Any, Any, T]) -> T:
+        """
+        Runs the coroutine in a new thread and waits for it to finish
+
+        :param coro: Coroutine
+        :return: T
+        """
+        result: Optional[T] = None
+        exception: Optional[Exception] = None
+
+        def target() -> None:
+            nonlocal result, exception
+            try:
+                result = asyncio.run(coro)
+            except Exception as e:
+                exception = e
+
+        thread = threading.Thread(target=target)
+        thread.start()
+        thread.join()
+
+        if exception:
+            raise exception
+
+        # Allow returning None without checking since T may be an Optional type already
+        return result  # type: ignore[return-value]
