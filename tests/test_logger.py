e98cc39411be34627b73d61d76d92943e3c53d4e
Imran Qureshi <imranq2@hotmail.com>
2024-11-02 23:17:01 -0700

    add test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,23 +1,37 @@
+import logging
+from logging import Logger
 from typing import Any
 
 from helix_fhir_client_sdk.loggers.fhir_logger import FhirLogger
 
 
 class TestLogger(FhirLogger):
+    def __init__(self) -> None:
+        self.logger: Logger = Logger("FhirLogger")
+        handler = logging.StreamHandler()
+        handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s %(module)s.%(funcName)s[%(lineno)d]: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        )
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
     def info(self, param: Any) -> None:
         """
         Handle messages at INFO level
         """
-        print(param)
+        self.logger.info(param)
 
     def error(self, param: Any) -> None:
         """
         Handle messages at error level
         """
-        print(param)
+        self.logger.error(param)
 
     def debug(self, param: Any) -> None:
         """
         Handle messages at debug level
         """
-        print(param)
+        self.logger.debug(param)
