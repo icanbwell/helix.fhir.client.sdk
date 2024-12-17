2ad295cbc013283a1440401fc5fb2a6f4ffbe54d
ChangSu Lee <changsu.lee@icanbwell.com>
2024-12-17 14:00:50 -0500

    updated w/ correct payload json with op, path, and value

--- helix_fhir_client_sdk/test/test_patch_mixin.py
+++ helix_fhir_client_sdk/test/test_patch_mixin.py
@@ -1,7 +1,7 @@
 import json
 
 import pytest
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from aioresponses import aioresponses
 
@@ -11,31 +11,35 @@ from helix_fhir_client_sdk.fhir_client import FhirClient
 @pytest.mark.asyncio
 async def test_patch_async_success() -> None:
     """Test successful patch_async call."""
-    url = "http://example.com/Observation/1"
+    url = "http://example.com/Observation"
     fhir_client = FhirClient().url(url).resource("Observation")
 
-    obs_updated_payload: Dict[str, Any] = {
-        "resourceType": "Observation",
-        "id": "1",
-        "code": {
-            "coding": [
-                {
-                    "id": "adbq8e47brct83v6",
-                    "extension": [
-                        {
-                            "id": "preferred",
-                            "url": "https://fhir.icanbwell.com/4_0_0/StructureDefinition/intelligence",
-                            "valueCode": "preferred",
-                        }
-                    ],
-                    "system": "http://hl7.org/fhir",
-                    "code": "HL7",
-                    "display": "HL7",
-                }
-            ]
-        },
-    }
-    print(json.dumps(obs_updated_payload))
+    obs_updated_payload: List[Dict[str, Any]] = [
+        {
+            "op": "replace",
+            "path": "/code",
+            "value": {
+                "coding": [
+                    {
+                        "id": "adbq8e47brct83v6",
+                        "extension": [
+                            {
+                                "id": "preferred",
+                                "url": "https://fhir.icanbwell.com/4_0_0/StructureDefinition/intelligence",
+                                "valueCode": "preferred",
+                            }
+                        ],
+                        "system": "http://hl7.org/fhir",
+                        "code": "HL7",
+                        "display": "HL7",
+                    }
+                ],
+                "text": "HL7",
+            },
+        }
+    ]
+    request_data_payload: str = json.dumps(obs_updated_payload)
+    print(request_data_payload)
 
     with aioresponses() as m:
         m.post(
@@ -47,10 +51,8 @@ async def test_patch_async_success() -> None:
         fhir_client._id = "1"
         fhir_client._validation_server_url = "http://validation-server.com"
 
-        response = await fhir_client.send_patch_request_async(
-            data=json.dumps(obs_updated_payload)
-        )
+        response = await fhir_client.send_patch_request_async(data=request_data_payload)
         print(response)
 
-        # assert response.status == 200
+        assert response.status == 200
         # assert response.responses[0]["resourceType"] == "Observation"
