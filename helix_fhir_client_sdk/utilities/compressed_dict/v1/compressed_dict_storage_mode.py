adae6c35cd129e9e2728fbac2d4886f2317bfb99
Imran Qureshi <imranq2@hotmail.com>
2025-04-01 19:00:46 -0700

    change default to compressed

--- helix_fhir_client_sdk/utilities/compressed_dict/v1/compressed_dict_storage_mode.py
+++ helix_fhir_client_sdk/utilities/compressed_dict/v1/compressed_dict_storage_mode.py
@@ -4,12 +4,47 @@ from typing import Literal, TypeAlias
 CompressedDictStorageType: TypeAlias = Literal[
     "raw", "compressed", "msgpack", "compressed_msgpack"
 ]
+"""
+CompressedDictStorageType is a type alias for the different storage types
+raw: No compression
+compressed: Compressed using zlib
+msgpack: Compressed using msgpack
+compressed_msgpack: Compressed using msgpack with zlib
+"""
 
 
 @dataclasses.dataclass(slots=True)
 class CompressedDictStorageMode:
+    """
+    CompressedDictStorageMode is a dataclass that defines the storage mode
+    """
+
     storage_type: CompressedDictStorageType = "compressed"
+    """
+    CompressedDictStorageType is a type alias for the different storage types
+    raw: No compression
+    compressed: Compressed using zlib
+    msgpack: Compressed using msgpack
+    compressed_msgpack: Compressed using msgpack with zlib
+    """
 
     @classmethod
     def default(cls) -> "CompressedDictStorageMode":
+        """
+        Returns the default storage mode
+        """
+        return cls(storage_type="compressed")
+
+    @classmethod
+    def raw(cls) -> "CompressedDictStorageMode":
+        """
+        Returns the default storage mode
+        """
+        return cls(storage_type="raw")
+
+    @classmethod
+    def compressed(cls) -> "CompressedDictStorageMode":
+        """
+        Returns the default storage mode
+        """
         return cls(storage_type="compressed")
