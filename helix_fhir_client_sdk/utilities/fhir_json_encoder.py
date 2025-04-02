c66600d4b465806dbb6d60c03ef2dee9cace4f64
Imran Qureshi <imranq2@hotmail.com>
2025-04-01 21:26:21 -0700

    add json encoder for Decimal

--- helix_fhir_client_sdk/utilities/fhir_json_encoder.py
+++ helix_fhir_client_sdk/utilities/fhir_json_encoder.py
@@ -1,6 +1,7 @@
 import dataclasses
 import json
 from datetime import datetime, date
+from decimal import Decimal
 from enum import Enum
 from typing import Any
 
@@ -11,8 +12,15 @@ class FhirJSONEncoder(json.JSONEncoder):
             return dataclasses.asdict(o)  # type:ignore
         if isinstance(o, Enum):
             return o.value
+        if isinstance(o, Decimal):
+            return str(o)
+        if isinstance(o, bytes):
+            return o.decode("utf-8")
+        # if isinstance(o, (datetime, date)):
+        #     return o.isoformat().replace("+00:00", ".000Z")
         if isinstance(o, (datetime, date)):
-            return o.isoformat().replace("+00:00", ".000Z")
+            return o.isoformat()
         if hasattr(o, "to_dict"):
             return o.to_dict()
+
         return super().default(o)
