876adaa249e8fe1a58bdcf75f7599be5855bcb59
Imran Qureshi <imranq2@hotmail.com>
2025-04-08 00:11:53 -0700

    switch cache to weak reference

--- helix_fhir_client_sdk/utilities/cache/request_cache_entry.py
+++ helix_fhir_client_sdk/utilities/cache/request_cache_entry.py
@@ -3,7 +3,7 @@ import dataclasses
 from helix_fhir_client_sdk.fhir.fhir_bundle_entry import FhirBundleEntry
 
 
-@dataclasses.dataclass(slots=True)
+@dataclasses.dataclass(slots=True, weakref_slot=True)
 class RequestCacheEntry:
     """
     Represents a cache entry for FHIR requests.
