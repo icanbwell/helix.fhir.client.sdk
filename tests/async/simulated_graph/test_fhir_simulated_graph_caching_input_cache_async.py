7ae691e02a9195e2e8975ca1c130f4a87acbe934
Imran Qureshi <imranq2@hotmail.com>
2025-04-14 16:33:21 -0700

    add test for if_modified_since

--- tests/async/simulated_graph/test_fhir_simulated_graph_caching_input_cache_async.py
+++ tests/async/simulated_graph/test_fhir_simulated_graph_caching_input_cache_async.py
@@ -21,7 +21,9 @@ async def test_fhir_simulated_graph_caching_input_cache_async() -> None:
     print("")
     data_dir: Path = Path(__file__).parent.joinpath("./")
     graph_json: Dict[str, Any]
-    with open(data_dir.joinpath("graphs").joinpath("provider.json"), "r") as file:
+    with open(
+        data_dir.joinpath("graphs").joinpath("provider_if_modified_since.json"), "r"
+    ) as file:
         contents = file.read()
         graph_json = json.loads(contents)
 
