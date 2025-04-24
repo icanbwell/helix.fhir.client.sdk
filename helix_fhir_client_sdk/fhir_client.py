43562365d9b403fe25b3c3a180064c78135cf1fa
Keshav Raj <keshav.raj@icanbwell.com>
2025-04-24 15:07:41 +0530

    PHR-864 removed limit from full_url query params

--- helix_fhir_client_sdk/fhir_client.py
+++ helix_fhir_client_sdk/fhir_client.py
@@ -675,8 +675,6 @@ class FhirClient(
             full_uri.args["_count"] = self._page_size
             # noinspection SpellCheckingInspection
             full_uri.args["_getpagesoffset"] = page_number or self._page_number
-        if not self._obj_id and (ids is None or self._filter_by_resource) and self._limit and self._limit >= 0:
-            full_uri.args["_count"] = self._limit
         # add any sort fields
         if self._sort_fields is not None:
             full_uri.args["_sort"] = ",".join([str(s) for s in self._sort_fields])
