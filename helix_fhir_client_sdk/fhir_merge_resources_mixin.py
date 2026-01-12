0c27bb4385e3b6161b43dfcc6959f90e082ad172
Akshay Kumar <akshay.kumar@icanbwell.com>
2026-01-12 23:36:07 +0530

    DCON-2481: Added _persistent_session

--- helix_fhir_client_sdk/fhir_merge_resources_mixin.py
+++ helix_fhir_client_sdk/fhir_merge_resources_mixin.py
@@ -343,6 +343,9 @@ class FhirMergeResourcesMixin(FhirClientProtocol):
                             log_all_url_results=self._log_all_response_urls,
                             access_token=self._access_token,
                             access_token_expiry_date=self._access_token_expiry_date,
+                            close_session_on_exit=self._close_session,
+                            persistent_session=self._persistent_session,
+                            use_persistent_session=self._use_persistent_session,
                         ) as client:
                             # should we check if it exists and do a POST then?
                             response: RetryableAioHttpResponse = await client.post(
